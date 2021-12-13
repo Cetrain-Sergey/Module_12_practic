ac7d89b02e6705e8dd234145d3a63881f1c4c44f
Cetrain <awaysin.info@gmail.com>
2021-12-14 01:22:26 +0300

    Threads

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        new NewThread("Один");
+        new NewThread("Два");
+        new NewThread("Три");
+
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+            System.out.println("Главный поток прерван");
+        }
+        System.out.println("Главный поток завершен");
+    }
+}
---
+++ src/NewThread.java
@@ -0,0 +1,23 @@
+public class NewThread implements Runnable{
+    String name;
+    Thread thread;
+
+    NewThread(String threadName){
+        name = threadName;
+        thread = new Thread(this, name);
+        System.out.println("Новый поток: " + thread);
+        thread.start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(name + " прерван");
+        }
+        System.out.println(name + " завершен");
+    }
+}
